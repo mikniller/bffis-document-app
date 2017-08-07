673e38891e1c92d5d3901b36286ef22dc18782f9
MikNiller <michael@cheta.dk>
2017-08-07 16:57:11 +0200

    Added cmd line param

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace bffisApp
 {
@@ -6,9 +7,33 @@ namespace bffisApp
     {
         static void Main(string[] args)
         {
+            // string doc = @"C:\Users\Michael\Dropbox\BFFIS\2017\HoldOversigt-ver1.xls";
+            string doc = "..\\HoldOversigt-ver1.xls";
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage is bffisApp.exe [fuld sti til excel fil] - e.g. bffisApp.exe c:\\dropbox\\bffis\\HoldOversigt-ver1.xls");
+                Console.WriteLine("Defaulter til : " + doc);
+            }
+            else
+            {
+                doc = args[1];
+            }
+            var path = Path.GetDirectoryName(doc);
+            var file = Path.GetFileName(doc);
+            // split into path and filename
+            Directory.SetCurrentDirectory(path);
+
+
+
+            // figure out where the xls is.
+
+
+
+
+
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
             DocumentReader reader = new DocumentReader();
-            var teams = reader.Read("HoldOversigt-ver1.xls");
+            var teams = reader.Read(file);
             foreach(var t in teams) {
                 Console.Write(t.ToString());
             }
